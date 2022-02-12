680507c943f9f7869649b70009693f6c8129dd7a
Gonzalo Díaz Excoffon <78400867+NuclearGeekBoy@users.noreply.github.com>
2022-02-11 21:26:51 -0300

    Create backstart.h

---
+++ backstart.h
@@ -0,0 +1,21 @@
+#ifndef BACKSTART
+#define BACKSTART
+
+/*STANDARD LIBRARIES*/
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/*BASIC DEFINITIONS*/
+#define BEGINNER 9 //modes for the game
+#define INTERMEDIATE 16
+#define EXPERT 31 //its width
+#define BMINE 10 //amount of mines for Beginner mode
+#define IMINE 40
+#define EMINE 99
+
+
+/*FUNCTS*/
+void print_clear(char,char,char mat[PRINCIPIANTE][PRINCIPIANTE]); //
+
+#endif
