123efaec587b14a99c3177b90274316dbfc851e2
Gonzalo Díaz Excoffon <78400867+NuclearGeekBoy@users.noreply.github.com>
2022-02-12 17:37:23 -0300

    Update backstart.h

--- code/backstart.h
+++ code/backstart.h
@@ -15,8 +15,23 @@
 #define IMINE 40
 #define EMINE 99
 
+/*STRUCTS FOR PUBLIC USE*/
+typedef struct 
+{
+    uint16_t tiles; //when it reaches 0, the game is over. You discovered every tile bombless
+    uint8_t bombs; //bomb counter to show during game
+    time_t time; // 0s to 999s
+}game_var; //game variables, every game may be different
+
+typedef struct
+{
+    uint16_t length; //mat size
+    uint16_t width;
+    uint8_t bombs; //initial bomb amount
+}game_invar; //game constants, invariables,
+
 
 /*FUNCTS*/
-void print_clear(char,char,char mat[PRINCIPIANTE][PRINCIPIANTE]); //
+void print_clear(char,char,char mat[BEGINNER][BEGINNER]); //
 
 #endif
