071f9aabba2ff3556949f14edb2e431b70b019ff
Gonzalo Díaz Excoffon <78400867+NuclearGeekBoy@users.noreply.github.com>
2022-02-13 02:56:01 -0300

    Update backstart.c

--- code/backstart.c
+++ code/backstart.c
@@ -31,14 +31,28 @@ typedef struct
 
 //functs
 void print_clear(char,char,tile_t mat[size_mat][size_mat]);
+void init_mat(tile_t [size_mat][size_mat]);
+
 
 int main()
 {
     tile_t mat[size_mat][size_mat];
+    init_mat(mat);
     print_clear(1,size_mat,mat);
+
 }
 
-/*FUNCTS DEFINITIONS*/
+void init_mat(tile_t mat[size_mat][size_mat])
+{
+    int cont_x, cont_y;
+    for(cont_y = 0 ; cont_y < size_mat ; cont_y++){
+            for(cont_x = 0 ; cont_x < size_mat ; cont_x++){
+                mat[cont_x][cont_y].off_vision = 0;
+            }
+        }
+}
+
+
 void print_clear(char clear,char size_mat,tile_t mat[size_mat][size_mat])
 {
     char cont_x,cont_y;
@@ -54,7 +68,7 @@ void print_clear(char clear,char size_mat,tile_t mat[size_mat][size_mat])
 
     for(cont_y = 0 ; cont_y < size_mat ; cont_y++){
         for(cont_x = 0 ; cont_x < size_mat ; cont_x++){
-            printf("| %d ",mat[cont_x][cont_y].in_vision);
+            printf("| %d ",mat[cont_x][cont_y].off_vision);
             if(cont_x == size_mat -1)
             {
                 printf("|\n");
