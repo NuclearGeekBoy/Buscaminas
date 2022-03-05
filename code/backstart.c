458ccf6b5e5222315e894c38cc929bca416d6572
Gonzalo Díaz Excoffon <78400867+NuclearGeekBoy@users.noreply.github.com>
2022-03-05 16:47:55 -0300

    Update backstart.c
    solved print bug XDDD

--- code/backstart.c
+++ code/backstart.c
@@ -10,27 +10,34 @@ void init_mat(tile_t mat[size_mat][size_mat])
         }
 }
 
-void print_clear(char clear,char size_mat,tile_t mat[size_mat][size_mat])
+void print_clear(game_invar game_data,tile_t *tile)
 {
-    char cont_x,cont_y;
+    char cont_y,cont_x;
+    tile_t *aux;
+    aux = tile; //aux pointer to storage the start of the mat
     
-    if(clear)
-    {
-        for(cont_y = 0 ; cont_y < size_mat ; cont_y++){
-            for(cont_x = 0 ; cont_x < size_mat ; cont_x++){
-                mat[cont_x][cont_y].in_vision = 0;
-            }
-        }
-    }
-
-    for(cont_y = 0 ; cont_y < size_mat ; cont_y++){
-        for(cont_x = 0 ; cont_x < size_mat ; cont_x++)
-        {
-            printf("| %d ",mat[cont_x][cont_y].in_vision);
-            if(cont_x == size_mat -1)
+    printf("off vision:\n");
+    for(cont_y = 0 ; cont_y < game_data.width ; cont_y++){
+        for(cont_x = 0 ; cont_x < game_data.length ; cont_x++){
+            printf("| %d ",tile->off_vision);
+            if(cont_x == game_data.length -1)
             {
                 printf("|\n");
             }
+            tile++;
+        }
+    }
+    tile = aux; //now i use it in order to printf the 'in vision' mat
+    
+    printf("\nin vision:\n");
+    for(cont_y = 0 ; cont_y < game_data.width ; cont_y++){
+        for(cont_x = 0 ; cont_x < game_data.length ; cont_x++){
+            printf("| %d ",tile->in_vision);
+            if(cont_x == game_data.length-1)
+            {
+                printf("|\n");
+            }
+            tile++;
         }
     }
 }
